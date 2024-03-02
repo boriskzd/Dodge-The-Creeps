d08bd2af4b36590966bd7d5e592565e4936302f7
Boris Knezevic <boris.knezevic@galeracluster.com>
2024-03-02 21:17:51 +0100

    make player movable, up down L R and diagonal

---
+++ Player.gd
@@ -0,0 +1,34 @@
+# https://docs.godotengine.org/en/stable/getting_started/first_2d_game/03.coding_the_player.html
+extends Area2D
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	# delta is frame length - the amount of time that previous frame took to complete
+	# using this value ensures that player movement will remain consistent even if the frame rate changes
+	position += velocity * delta 
+	# clamping value means restricting it to a given range
+	# ensures that player doesn't leave the screen
+	position = position.clamp(Vector2.ZERO, screen_size) 
